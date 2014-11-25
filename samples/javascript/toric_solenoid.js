5b336d4d1edbf6a0dc31193b71fb026c369ed1b6
jeesay <jjeesay@gmail.com>
2014-11-25 15:12:17 +0100

    Create toric_solenoid.js

---
+++ samples/javascript/toric_solenoid.js
@@ -0,0 +1,7 @@
+// Toric solenoid
+// Jean-Christophe Taveau
+// Formulas from http://www.mathcurve.com/courbes3d/solenoidtoric/solenoidtoric.shtml
+// http://crazybiocomputing.blogspot.com
+
+// TODO
+
