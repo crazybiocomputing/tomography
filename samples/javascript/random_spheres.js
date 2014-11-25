3300e81950dfb8ce526f09ab73437ea127a75d0b
jeesay <jjeesay@gmail.com>
2014-11-25 15:11:43 +0100

    Create random_spheres.js

---
+++ samples/javascript/random_spheres.js
@@ -0,0 +1,6 @@
+// Toric solenoid
+// Jean-Christophe Taveau
+// Formulas from http://www.mathcurve.com/courbes3d/solenoidtoric/solenoidtoric.shtml
+// http://crazybiocomputing.blogspot.com
+
+// T O D O
